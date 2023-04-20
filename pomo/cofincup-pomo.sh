4535e14fb2f5a82b85a47a203eed9d7ff904893d
cofincup <201220081@nju.edu.cn>
2023-04-20 11:52:27 +0800

    update cofincup-pomo

---
+++ pomo/cofincup-pomo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# get user input in minutes
+read -p "Enter countdown time in minutes: " countdown_minutes
+
+# confirm countdown time with user
+read -p "Start a countdown of $countdown_minutes minutes? [Y/n]" confirm
+confirm=${confirm:-Y} # set default value to Y if user presses enter without entering a value
+
+# exit script if user doesn't confirm countdown
+if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
+  echo "Countdown canceled."
+  exit 0
+fi
+
+# convert minutes to seconds
+countdown_time=$((countdown_minutes * 60))
+
+# ignore hangup signal and continue running in the background
+nohup bash -c "sleep $countdown_time && paplay /usr/share/sounds/freedesktop/stereo/complete.oga && notify-send 'Countdown complete!' 'The countdown has finished.'" > /dev/null 2>&1 & disown
+
